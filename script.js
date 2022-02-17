3e177beadb976e2def507f8562e28ec767f63fc5
tanianasrin1 <tasrineva169@gmail.com>
2022-02-17 08:33:57 +0600

    input value

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('total-calc').addEventListener('click',function(){
+    const input = document.getElementById('total-income');
+    const amount = input.value
+   
+    const total = document.getElementById('balance-total')
+    total.innerText = amount;
+    input.value = '';
+
+    
+})
+
+document.getElementById('total-calc').addEventListener('click', function(){
+    const foodInput = document.getElementById('foot-input').value
+    const foodAmount = parseFloat(foodInput);
+    const rentInput = document.getElementById('rent-input').value
+    const rentAmount = parseFloat(rentInput)
+    const othersInput = document.getElementById('others-input').value
+    const othersAmount = parseFloat(othersInput)
+    const totalCost = foodAmount + rentAmount + othersAmount
+   const expensiveTotal = document.getElementById('total-expensive')
+   expensiveTotal.innerText = totalCost
+
+
+})
+
