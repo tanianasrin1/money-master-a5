bf1637d697fc4e7a2d3dc3819b60fec53f5e58ce
tanianasrin1 <tasrineva169@gmail.com>
2022-02-17 22:01:13 +0600

    erre handling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 document.getElementById('total-calc').addEventListener('click', function(){
+    
+   
 
+    //    monthly income cost
     const foodInput = document.getElementById('foot-input').value
     const foodAmount = parseFloat(foodInput);
     const rentInput = document.getElementById('rent-input').value
@@ -10,17 +13,72 @@ document.getElementById('total-calc').addEventListener('click', function(){
    const expensiveTotal = document.getElementById('total-expensive')
    expensiveTotal.innerText = totalCost
 
-   const input = document.getElementById('total-income').value
+//    monthly income input
+   const inputincome = document.getElementById('total-income').value
    const balanceTotal = document.getElementById('balance-total')
-   balanceTotal.innerText = input - totalCost
-   
-   
-    
+   balanceTotal.innerText = inputincome - totalCost
 
+
+//    error handling negative value
+   if(foodInput <0 ){
+       alert('please enter the valid number')
+   }
+   if(rentInput <0 ){
+       alert('please enter the valid number')
+   }
+   if(othersInput <0 ){
+       alert('please enter the valid number')
+   }
+
+   if(inputincome <0 ){
+       alert('please enter the valid number')
+   }
+
+//   empty string error
+   if(foodInput == ''){
+       alert('please enter the valid number')
+   }
+   if(rentInput == ''){
+       alert('please enter the valid number')
+   }
+   if(othersInput == ''){
+       alert('please enter the valid number')
+   }
+
+   if(inputincome == ''){
+       alert('please enter the valid number')
+   }
+
+//  error handling by string
+   
+   if(isNaN(inputincome)){
+       alert('please enter the valid input')
+   }
+   if(isNaN(foodInput)){
+       alert('please enter the valid input')
+   }
+   if(isNaN(rentInput)){
+       alert('please enter the valid input')
+   }
+   if(isNaN(othersInput)){
+       alert('please enter the valid input')
+   }
+
+  
+ 
 })
+
 //  saving part
 document.getElementById('save-button').addEventListener('click', function(){
-    const saveInput = document.getElementById('save-input').value
-    const saveParcentage = document.getElementById('saving-parcentage')
-    saveParcentage.innerText =  saveInput/ 10
+    const incomeInput = document.getElementById('total-income').value
+    const incomeAmount = parseFloat(incomeInput);
+    const savingInput = document.getElementById('save-input').value
+    const savingAmount = parseFloat(savingInput)
+    // const pertange = (incomeAmount / 100) *10 
+    //  savingAmount.innerText = pertange
+    
+    console.log(savingInput)
+    
+
+ 
 })
